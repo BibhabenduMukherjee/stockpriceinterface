79e073d5abce3b9c6e4dc91509164d5eb7bc4ca3
= <mukherjee4004@gmail.com>
2024-03-11 10:43:36 +0530

    test src/app/(root)/testconvex/page.tsx && update convex email status logic add send email function

--- convex/emails.ts
+++ convex/emails.ts
@@ -1,22 +1,66 @@
-import { mutation, internalMutation } from "./_generated/server";
+import { mutation, internalMutation, query } from "./_generated/server";
 import { internal } from "./_generated/api";
+
 import { v } from "convex/values";
+//import EmailTemplate from "../src/app/components/EmailTemplate.tsx"
+export const getUserEmailStat = query({
+  args: {
+    userId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("useremailcount")
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .collect();
+  },
+});
+
+export const updateEmailSts = mutation({
+  args: {
+    id: v.id("useremailcount"),
+  },
+  handler: async (ctx, args) => {
+    const { id } = args;
+    await ctx.db.patch(id, { emailSts: true });
+  },
+});
 
 export const sendFirstTrainingSuccess = mutation({
-    args: { userId : v.string()},
-    handler: async (ctx, args) => {
-      const { userId } = args;
-      // set 1 for the user
-      const isPresent = await ctx.db.query("useremailcount").collect()
-      //if( isPresent.length > 0) return ;
-      
-      await ctx.db.insert("useremailcount" , { userId: userId, count : 1})
-      // go for the insertion
-      const id = await ctx.db.insert("firsttrainingemails", { userId });
-      
-    //   await ctx.scheduler.runAfter(5000, internal.messages.destruct, {
-    //     messageId: id,
-    //   });
-    },
-  });
-  
+  args: { userId: v.string() },
+  handler: async (ctx, args) => {
+    const { userId } = args;
+    // Check the user is already present
+    const isPresent = await ctx.db
+      .query("useremailcount")
+      .filter((q) => q.eq(q.field("userId"), userId))
+      .collect();
+    if (isPresent.length > 0) return;
+    // insert the record with false emailSts
+    await ctx.db.insert("useremailcount", {
+      userId: userId,
+      count: 1,
+      emailSts: false,
+    });
+    // send the same record
+    return await ctx.db
+      .query("useremailcount")
+      .filter((q) => q.eq(q.field("userId"), userId))
+      .collect();
+  },
+});
+
+// export const send = internalMutation({
+//   args: {},
+//   handler: async (ctx, args) => {
+//     const { data, error } = await resend.emails.send({
+//       from: "Acme <onboarding@resend.dev>",
+//       to: ["mukherjee4004@gmail.com"],
+//       subject: "Hello world",
+//       text: "hello",
+//     });
+
+//     if (error) {
+//       console.log(error);
+//     }
+//   },
+// });
--- src/app/(root)/testconvex/page.tsx
+++ src/app/(root)/testconvex/page.tsx
@@ -1,20 +1,50 @@
-"use client"
+"use client";
 
-import { api } from '@/convex/_generated/api'
-import { useMutation } from 'convex/react'
-import React from 'react'
+import { api } from "@/convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
+import React, { useEffect } from "react";
 
 function page() {
- const sendFirstTrainingSuccess = useMutation(api.emails.sendFirstTrainingSuccess)
+  const sendFirstTrainingSuccess = useMutation(
+    api.emails.sendFirstTrainingSuccess
+  );
+  const userInfo = useQuery(api.emails.getUserEmailStat, { userId: "12463" });
+  const updateEmailSts = useMutation(api.emails.updateEmailSts);
+  //console.log(userInfo);
 
- 
+  useEffect(() => {
+    if (userInfo) {
+      const user = userInfo[0];
+      if (user) {
+        if (user.emailSts === false) {
+          // send the email
+
+          updateEmailSts({ id: user._id });
+        }
+      }
+    }
+  }, [userInfo]);
+
+  //console.log();
+
   return (
     <div>
+      {userInfo &&
+        userInfo.map((item) => (
+          <>
+            <p key={item._id}>{item.userId}</p>
+          </>
+        ))}
 
-      <button onClick={()=>{sendFirstTrainingSuccess({userId : "12463"})}}>Click</button>
-
+      <button
+        onClick={() => {
+          sendFirstTrainingSuccess({ userId: "12463" });
+        }}
+      >
+        Click
+      </button>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
